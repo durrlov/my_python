91b807be771285eeaca80c5b349c750ff7f2eae0
durrlov <110600698+durrlov@users.noreply.github.com>
2022-08-04 16:12:53 +0600

    Create test.py
    test code with input

---
+++ test.py
@@ -0,0 +1,18 @@
+#Sum of squares(without x)
+
+
+def ss(x):
+  sum = 0
+  
+  x = int(x)
+  
+  for i in range(x):
+    sqr = i*i
+    sum += sqr
+  return(sum)
+
+print("Sum of squares")
+print('Enter a number')
+x = input()
+print(ss(x))
+
